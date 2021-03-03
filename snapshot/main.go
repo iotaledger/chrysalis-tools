f56c4a46d339439b37a7e175baf832cfa4b2cde3
Luca Moser <moser.luca@gmail.com>
2021-03-03 11:50:38 +0100

    sort ledger entries to produce the same files over muliple runs for the same ledger state

--- snapshot/main.go
+++ snapshot/main.go
@@ -19,6 +19,7 @@ import (
 	"github.com/iotaledger/iota.go/address"
 	"github.com/iotaledger/iota.go/api"
 	"github.com/iotaledger/iota.go/consts"
+	"github.com/iotaledger/iota.go/trinary"
 	"github.com/iotaledger/iota.go/v2"
 	"github.com/spf13/pflag"
 	"golang.org/x/crypto/blake2b"
@@ -30,6 +31,7 @@ var (
 	globalSnapshotFileName       = pflag.String("global-snapshot-file", "global_snapshot.csv", "the name of the global snapshot file to generate")
 	genesisSnapshotFileName      = pflag.String("genesis-snapshot-file", "genesis_snapshot.bin", "the name of the genesis snapshot file to generate")
 	genesisSnapshotFileNetworkID = pflag.String("genesis-snapshot-file-network-id", "mainnet1", "the network ID to put into the genesis snapshot")
+	genesisSnapshotTimestamp     = pflag.Uint64("genesis-snapshot-file-timestamp", 0, "the timestamp to use for the genesis snapshot")
 )
 
 func must(err error) {
@@ -64,44 +66,62 @@ func main() {
 	must(err)
 
 	log.Printf("total ledger entries: %d", len(resObj.Balances))
-	migrations := make(map[[32]byte]uint64)
+	type migration struct {
+		ed25519Addr [32]byte
+		value       uint64
+	}
+	var migrations []migration
 	var totalMigration uint64
 	var eligibleAddrsForMigration, eligibleAddrsTokensTotal uint64
-	var hashInput []string
 
 	globalSnapshotFile, err := os.OpenFile(*globalSnapshotFileName, os.O_TRUNC|os.O_CREATE|os.O_RDWR, os.ModePerm)
 	must(err)
 	defer globalSnapshotFile.Close()
 
+	type legacyLedgerEntry struct {
+		addr    trinary.Hash
+		balance uint64
+	}
+
+	var legacyLedgerEntries []legacyLedgerEntry
 	for addr, balance := range resObj.Balances {
-		hashInput = append(hashInput, fmt.Sprintf("%s%d", addr, balance))
+		legacyLedgerEntries = append(legacyLedgerEntries, legacyLedgerEntry{
+			addr:    addr,
+			balance: balance,
+		})
+	}
+
+	sort.Slice(legacyLedgerEntries, func(i, j int) bool {
+		return legacyLedgerEntries[i].addr < legacyLedgerEntries[j].addr
+	})
+
+	legacyLedgerEntriesHash, err := blake2b.New256(nil)
+	must(err)
+	for _, entry := range legacyLedgerEntries {
+		legacyLedgerEntriesHash.Write([]byte(fmt.Sprintf("%s%d", entry.addr, entry.balance)))
 
 		// write to global snapshot file
-		_, err := fmt.Fprintf(globalSnapshotFile, "%s;%d\n", addr, balance)
+		_, err := fmt.Fprintf(globalSnapshotFile, "%s;%d\n", entry.addr, entry.balance)
 		must(err)
 
-		if ed25519Addr, err := address.ParseMigrationAddress(addr); err == nil {
-			if balance < *minMigratedFundsAmount {
+		if ed25519Addr, err := address.ParseMigrationAddress(entry.addr); err == nil {
+			if entry.balance < *minMigratedFundsAmount {
 				continue
 			}
-			migrations[ed25519Addr] = balance
-			totalMigration += balance
+			migrations = append(migrations, migration{
+				ed25519Addr: ed25519Addr,
+				value:       entry.balance,
+			})
+			totalMigration += entry.balance
 			continue
 		}
-		if balance >= *minMigratedFundsAmount {
+		if entry.balance >= *minMigratedFundsAmount {
 			eligibleAddrsForMigration++
-			eligibleAddrsTokensTotal += balance
+			eligibleAddrsTokensTotal += entry.balance
 		}
 	}
 
-	sort.Strings(hashInput)
-	hash, err := blake2b.New256(nil)
-	must(err)
-	for _, s := range hashInput {
-		hash.Write([]byte(s))
-	}
-
-	log.Println("ledger state integrity hash:", hex.EncodeToString(hash.Sum(nil)))
+	log.Println("ledger state integrity hash:", hex.EncodeToString(legacyLedgerEntriesHash.Sum(nil)))
 	log.Printf("migration: addrs %d, tokens total %d", len(migrations), totalMigration)
 	log.Printf("eligible for migration: addrs %d, tokens total %d", eligibleAddrsForMigration, eligibleAddrsTokensTotal)
 
@@ -119,7 +139,7 @@ func main() {
 	var outputIndex uint16
 	fakeTransactionID := [32]byte{}
 	var fakeTransactionIDBoundary uint16
-	for ed25519, balance := range migrations {
+	for _, migration := range migrations {
 		if outputIndex == iota.MaxOutputsCount {
 			outputIndex = 0
 			fakeTransactionIDBoundary++
@@ -135,11 +155,11 @@ func main() {
 			MessageID:  [32]byte{},
 			OutputID:   outputID,
 			OutputType: 0,
-			Amount:     balance,
+			Amount:     migration.value,
 		}
 
 		edSeri := &iota.Ed25519Address{}
-		copy(edSeri[:], ed25519[:])
+		copy(edSeri[:], migration.ed25519Addr[:])
 		output.Address = edSeri
 
 		migrationOutputs = append(migrationOutputs)
@@ -147,7 +167,8 @@ func main() {
 	}
 
 	var currentOutput int
-	must(snapshot.StreamSnapshotDataTo(genesisSnapshotFile, uint64(time.Now().Unix()), &snapshot.FileHeader{
+
+	must(snapshot.StreamSnapshotDataTo(genesisSnapshotFile, *genesisSnapshotTimestamp, &snapshot.FileHeader{
 		Version:              snapshot.SupportedFormatVersion,
 		Type:                 0,
 		NetworkID:            iota.NetworkIDFromString(*genesisSnapshotFileNetworkID),
