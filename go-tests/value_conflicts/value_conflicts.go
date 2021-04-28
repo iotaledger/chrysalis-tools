d07e4ce359e26df66ed98f98216f9f062c3e9411
galrogo <gal.rogozinski@iota.org>
2021-04-28 15:06:09 +0300

    fix value_conflicts

--- go-tests/value_conflicts/value_conflicts.go
+++ go-tests/value_conflicts/value_conflicts.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	iota "github.com/GalRogozinski/iota.go/v2"
 	. "github.com/iotaledger/chrysalis-tools/go-tests/lib"
+	"log"
+	"time"
 )
 
 func main() {
@@ -12,7 +14,7 @@ func main() {
 
 	nodeAPI, nodeInfo := ObtainAPI(*nodeDomain, *apiPort)
 
-	genesisAddress := iota.Ed25519Address{}
+	//genesisAddress := iota.Ed25519Address{}
 	genesisOutput := [iota.TransactionIDLength]byte{}
 
 	seed := CreateSeed([]byte{0xde, 0xad, 0xbe, 0xef})
@@ -22,21 +24,42 @@ func main() {
 		Keys:    privateKey,
 	})
 
+	seed = CreateSeed([]byte{0xde, 0xad, 0xbe, 0xea})
+	_, _, address2 := GenerateAddressFromSeed(seed)
+
+	seed = CreateSeed([]byte{0xae, 0xad, 0xbe, 0xea})
+	_, _, address3 := GenerateAddressFromSeed(seed)
+
+	balanceResp, err := nodeAPI.BalanceByEd25519Address(&address1)
+	Must(err)
+	amount := balanceResp.Balance
+
 	tx, err := iota.NewTransactionBuilder().
-		AddInput(CreateInput(&genesisAddress, genesisOutput, 0)).
-		AddOutput(CreateOutput(&address1, 1_500_000)).
+		AddInput(CreateInput(&address1, genesisOutput, 0)).
+		AddOutput(CreateOutput(&address2, amount)).
 		AddIndexationPayload(&iota.Indexation{Index: []byte("value"), Data: []byte("test")}).
 		Build(signer)
 	Must(err)
 
 	SendValueMessage(nodeAPI, &nodeInfo.NetworkID, nil, tx)
+	log.Println("sent value to ", address2.Bech32("atoi"))
 
 	tx, err = iota.NewTransactionBuilder().
-		AddInput(CreateInput(&genesisAddress, genesisOutput, 0)).
-		AddOutput(CreateOutput(&address1, 1_800_000)).
+		AddInput(CreateInput(&address1, genesisOutput, 0)).
+		AddOutput(CreateOutput(&address3, amount)).
 		AddIndexationPayload(&iota.Indexation{Index: []byte("value"), Data: []byte("test")}).
 		Build(signer)
 	Must(err)
 
 	SendValueMessage(nodeAPI, &nodeInfo.NetworkID, nil, tx)
+	log.Println("sent value to ", address3.Bech32("atoi"))
+
+	time.Sleep(11 * time.Second)
+	addressResp, err := nodeAPI.BalanceByEd25519Address(&address2)
+	Must(err)
+	log.Printf("balance in %s is %d", address2.Bech32("atoi"), addressResp.Balance)
+
+	addressResp, err = nodeAPI.BalanceByEd25519Address(&address3)
+	Must(err)
+	log.Printf("balance in %s is %d", address3.Bech32("atoi"), addressResp.Balance)
 }
